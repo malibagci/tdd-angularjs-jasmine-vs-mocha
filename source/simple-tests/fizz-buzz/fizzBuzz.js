3679209a360be242763f5a83d564894f20eb4508
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-04-08 13:04:45 +0200

    clean up fizz buzz

--- source/simple-tests/fizz-buzz/fizzBuzz.js
+++ source/simple-tests/fizz-buzz/fizzBuzz.js
@@ -1,27 +1,16 @@
-describe('let\'s play FizzBuzz', function() {
+var playFizzBuzz = function(withNumber) {
 
-  it('should convert 1 to 1', function() {
-    expect(playFizzBuzz(1)).toEqual(1);
-  });
+  if((withNumber % 3) === 0 && (withNumber % 5) === 0) {
+    return 'FizzBuzz';
+  }
 
-  it('should convert 3 to "Fizz"', function() {
-    expect(playFizzBuzz(3)).toEqual('Fizz');
-  });
+  if((withNumber % 3) === 0) {
+    return 'Fizz';
+  }
 
-  it('should convert 5 to "Buzz"', function() {
-    expect(playFizzBuzz(5)).toEqual('Buzz');
-  });
-
-  it('should convert 9 to "Fizz"', function() {
-    expect(playFizzBuzz(9)).toEqual('Fizz');
-  });
-
-  it('should convert 10 to "Buzz"', function() {
-    expect(playFizzBuzz(10)).toEqual('Buzz');
-  });
-
-  it('should convert 15 to "FizzBuzz"', function() {
-    expect(playFizzBuzz(15)).toEqual('FizzBuzz');
-  });
-
-});
+  if((withNumber % 5) === 0) {
+    return 'Buzz';
+  }
+  
+  return withNumber;
+};
