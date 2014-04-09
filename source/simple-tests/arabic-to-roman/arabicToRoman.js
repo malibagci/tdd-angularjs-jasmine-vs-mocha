3d70d6e4ef415fe6941bef60d1b6c5084854821a
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-04-09 17:28:53 +0200

    updated arabic to roman numerals and added tdd example arabic to roman numerals to thesis

--- source/simple-tests/arabic-to-roman/arabicToRoman.js
+++ source/simple-tests/arabic-to-roman/arabicToRoman.js
@@ -10,32 +10,29 @@
 
 var arabic_to_roman = function(input) {
 
-  var edge_cases = {
-    "1000": "M",
-    "900": "CM",
-    "500": "D",
-    "400": "CD",
-    "100": "C",
-    "90": "XC",
-    "50": "L",
-    "40": "XL",
-    "10": "X",
-    "9": "IX",
-    "5": "V",
-    "4": "IV"
+  var lookUp = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
   };
 
-  for(var arabic in edge_cases) {
-    if(input >= arabic)
-      return edge_cases[arabic] + arabic_to_roman(input - arabic);
+  for(var arabic in lookUp) {
+    if(input >= arabic) {
+      return lookUp[arabic] + arabic_to_roman(input - arabic);
+    }
   }
-
-  var ret = "";
-  for(var i = 0; i < input; ++i) {
-    ret += "I";
-  }
-
-  return ret;
+  
+  return "";
 
 };
 
