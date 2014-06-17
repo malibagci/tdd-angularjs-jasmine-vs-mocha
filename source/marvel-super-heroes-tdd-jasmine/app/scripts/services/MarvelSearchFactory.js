f39d609dd79adcfe20c566534caba7c63de802ab
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 10:58:08 +0200

    tdd-jasmine: added MarvelSearchFactory; green phase;

---
+++ source/marvel-super-heroes-tdd-jasmine/app/scripts/services/MarvelSearchFactory.js
@@ -0,0 +1,12 @@
+// app/scripts/services/MarvelSearchFactory.js
+
+'use strict';
+
+angular.module( 'marvelSuperHeroesApp' )
+  .factory( 'MarvelSearchFactory', function() {
+
+    var factory = {};
+
+    return factory;
+
+  });
