459be2e305659c754ddee6f2604b9c3507923567
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 11:15:14 +0200

    tdd-jasmine: added HTTP-GET in MarvelSearchFactory; green phase;

--- source/marvel-super-heroes-tdd-jasmine/app/scripts/services/MarvelSearchFactory.js
+++ source/marvel-super-heroes-tdd-jasmine/app/scripts/services/MarvelSearchFactory.js
@@ -3,12 +3,21 @@
 'use strict';
 
 angular.module( 'app.services' )
-  .factory( 'MarvelSearchFactory', function() {
+  .factory( 'MarvelSearchFactory', function MarvelSearchFactory($http) {
 
     var factory = {};
+    var apikey = "28969060faef0943a7c866a98e465269";
 
     factory.searchResults = [];
-    factory.search = function() {};
+    factory.search = function() {
+
+      var fetch = $http({
+        method: 'GET',
+        url: 'http://gateway.marvel.com:80/v1/public/characters?' +
+          'limit=20' + '&nameStartsWith=Hulk' + '&apikey=' + apikey
+      });
+
+    };
 
     return factory;
 
