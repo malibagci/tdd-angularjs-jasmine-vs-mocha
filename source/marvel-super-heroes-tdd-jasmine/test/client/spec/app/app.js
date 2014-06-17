26532262064ee5975675e1cdb05f10047ec6f6fe
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 10:17:28 +0200

    tdd-jasmine: added regression test for app dependency to ngRoute; still green phase;

--- source/marvel-super-heroes-tdd-jasmine/test/client/spec/app/app.js
+++ source/marvel-super-heroes-tdd-jasmine/test/client/spec/app/app.js
@@ -5,12 +5,23 @@ describe( 'Testing modules:', function() {
   describe( 'App module:', function() {
 
     var module;
+    var deps;
+
+    var hasModule = function(moduleName) {
+      return deps.indexOf(moduleName) >= 0;
+    };
+
     beforeEach( function() {
       module = angular.module('marvelSuperHeroesApp');
+      deps = module.value('marvelSuperHeroesApp').requires;
     });
 
-    it('should be registered', function() {
+    it( 'should be registered', function() {
       expect(module).toBeDefined();
+    });
+
+    it( 'should have a registered dependency to ngRoute', function() {
+      expect(hasModule('ngRoute')).toBeTruthy();
     });
 
   });
