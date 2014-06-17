f7045189162eeb05f497c3a3bf85686ae0fc816d
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 20:15:56 +0200

    tdd-mocha: added HTTP-GET to MarvelSearchFactory; green phase;

--- source/marvel-super-heroes-tdd-mocha/app/scripts/services/MarvelSearchFactory.js
+++ source/marvel-super-heroes-tdd-mocha/app/scripts/services/MarvelSearchFactory.js
@@ -1,12 +1,20 @@
 // app/scripts/services/MarvelSearchFactory.js
 
 angular.module( 'app.services' )
-  .factory( 'MarvelSearchFactory', function() {
+  .factory( 'MarvelSearchFactory', function($http) {
+    
     var factory = {};
+    var apikey = "28969060faef0943a7c866a98e465269";
 
     factory.searchResults = [];
 
-    factory.search = function() {};
+    factory.search = function() {
+      $http({
+        method: 'GET',
+        url: 'http://gateway.marvel.com:80/v1/public/characters?' +
+          'limit=20' + '&nameStartsWith=Hulk' + '&apikey=' + apikey
+      });
+    };
 
     return factory;
   });
