2315ef3ed520063e7a9a021468b72fc8059551d8
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 19:21:17 +0200

    tdd-mocha: added templateUrl-test for root route; red phase;

--- source/marvel-super-heroes-tdd-mocha/test/client/spec/app/route.js
+++ source/marvel-super-heroes-tdd-mocha/test/client/spec/app/route.js
@@ -2,18 +2,26 @@
 
 suite('Testing App Routes:', function() {
 
+  var $route;
+
   setup( function() {
     module( 'marvelSuperHeroesApp' );
-  });
-
-  // suite( 'Testing Root Route:', function() {
-
-  test( 'should validate, that the root route "/" is present', function() {
-    inject( function($route) {
-      assert.isDefined($route.routes['/']);
+    inject( function(_$route_) {
+      $route = _$route_;
     });
   });
 
-  // });
+  suite( 'Testing Root Route:', function() {
+
+    test( 'if the root route "/" is present', function() {
+      assert.isDefined($route.routes['/']);
+    });
+
+    test( 'if the root route has the correct templateUrl ("partials/heroes")', 
+      function() {
+        assert.equal($route.routes['/'].templateUrl, 'partials/heroes');
+    });
+
+  });
 
 });
