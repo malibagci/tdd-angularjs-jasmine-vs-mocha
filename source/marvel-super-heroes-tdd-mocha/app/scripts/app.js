4edc0915f87b2cc99ec81004731fb6bbdc2d86e9
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 19:54:12 +0200

    tdd-mocha: added app-dependency-test for app.services; red phase;

--- source/marvel-super-heroes-tdd-mocha/app/scripts/app.js
+++ source/marvel-super-heroes-tdd-mocha/app/scripts/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module( 'app.controllers', [] );
+angular.module( 'app.services', [] );
 
 angular.module( 'marvelSuperHeroesApp', [
   'ngRoute',
-  'app.controllers'
+  'app.controllers',
+  'app.services'
 ]).config(function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'partials/heroes',
