e3f9f7eb65268462b29667ce1bb4959a53af63c4
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 12:48:43 +0200

    tdd-jasmine: added test for the data-binding of the heroes array in HeroesCtrl service, it should be binded to the heroes array of the HeroesFactory service; red phase;

--- source/marvel-super-heroes-tdd-jasmine/test/client/spec/controllers/HeroesCtrl.js
+++ source/marvel-super-heroes-tdd-jasmine/test/client/spec/controllers/HeroesCtrl.js
@@ -4,17 +4,24 @@ describe( 'Testing HeroesCtrl Controller:', function() {
 
   var HeroesCtrl,
     scope,
-    MarvelSearchFactory;
+    MarvelSearchFactory,
+    HeroesFactory;
 
   beforeEach( module('marvelSuperHeroesApp') );
 
-  beforeEach( inject( function($controller, $rootScope, _MarvelSearchFactory_) {
-    scope = $rootScope.$new();
-    MarvelSearchFactory = _MarvelSearchFactory_;
-    HeroesCtrl = $controller( 'HeroesCtrl', {
-      $scope: scope,
-      MarvelSearchFactory: MarvelSearchFactory
-    });
+  beforeEach( inject( function($controller, $rootScope, _MarvelSearchFactory_,
+    _HeroesFactory_) {
+
+      scope = $rootScope.$new();
+      MarvelSearchFactory = _MarvelSearchFactory_;
+      HeroesFactory = _HeroesFactory_;
+
+      HeroesCtrl = $controller( 'HeroesCtrl', {
+        $scope: scope,
+        MarvelSearchFactory: MarvelSearchFactory,
+        HeroesFactory: HeroesFactory
+      });
+
   }));
 
   it( 'should be present', function() {
@@ -59,6 +66,24 @@ describe( 'Testing HeroesCtrl Controller:', function() {
     it( 'should have a addToFavorites function', function() {
       expect(scope.addToFavorites).toBeDefined();
       expect(scope.addToFavorites).toEqual(jasmine.any(Function));
+    });
+
+    it( 'should update the heroes array, when the heroes array of the ' +
+      'HeroesFactory service has been changed', function() {
+
+        expect(scope.heroes.length).toEqual(0);
+        
+        HeroesFactory.heroes.push({
+          id: 1,
+          name: 'Hulk',
+          thumbnail: {
+            path: 'path_to_thumbnail',
+            extension: 'extension_of_thumbnail'
+          }
+        });
+
+        expect(scope.heroes.length).toEqual(1);
+
     });
 
   });
