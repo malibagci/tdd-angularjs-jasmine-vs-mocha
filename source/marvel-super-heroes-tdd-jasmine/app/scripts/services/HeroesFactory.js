cdff427c00cd824fc7c612d1c477cb424a3df94a
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 12:42:37 +0200

    tdd-jasmine: added HeroesFactory; green phase;

---
+++ source/marvel-super-heroes-tdd-jasmine/app/scripts/services/HeroesFactory.js
@@ -0,0 +1,10 @@
+// app/scripts/services/HeroesFactory.js
+
+angular.module( 'app.services' )
+  .factory( 'HeroesFactory', function HeroesFactory() {
+
+    var factory = {};
+
+    return factory;
+
+  });
