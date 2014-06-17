2f9bad5938b6078ecb2810a0c345aa4c3d1d35ef
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 11:11:31 +0200

    tdd-jasmine: added HTTP-GET test in MarvelSearchFactory; red phase;

--- source/marvel-super-heroes-tdd-jasmine/test/client/spec/services/MarvelSearchFactory.js
+++ source/marvel-super-heroes-tdd-jasmine/test/client/spec/services/MarvelSearchFactory.js
@@ -24,4 +24,51 @@ describe( 'Testing MarvelSearchFactory Service:', function() {
     expect(MarvelSearchFactory.search).toEqual(jasmine.any(Function));
   });
 
+  describe( 'Testing Marvel-API Search', function() {
+
+    var $httpBackend;
+
+    beforeEach( inject(function(_$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+
+      var apikey = '28969060faef0943a7c866a98e465269';
+      var requestURI = 'http://gateway.marvel.com:80/v1/public/characters';
+      requestURI += '?limit=20&nameStartsWith=Hulk&apikey=' + apikey;
+
+      $httpBackend.expectGET(requestURI).respond({
+        data: {
+          results: [
+            {
+              id: 1,
+              name: 'Hulk',
+              thumbnail: {
+                path: 'path_to_thumbnail',
+                extension: 'extension_of_thumbnail'
+              }
+            },
+            {
+              id: 2,
+              name: 'Hulkling',
+              thumbnail: {
+                path: 'path_to_thumbnail',
+                extension: 'extension_of_thumbnail'
+              }
+            }
+          ]
+        }
+      });
+
+    }));
+
+    it( 'should make a GET Request if search is called', function() {
+
+      MarvelSearchFactory.search();
+      $httpBackend.flush();
+
+    });
+
+
+  });
+
+
 });
