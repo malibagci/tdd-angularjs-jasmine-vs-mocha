6b5c3eb2017c0eb9c1ddf8c012b1ad22b2202a3d
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 19:34:52 +0200

    tdd-mocha: added NavbarCtrl test, test if it is present; red phase;

--- source/marvel-super-heroes-tdd-mocha/test/client/spec/controllers/NavbarCtrl.js
+++ source/marvel-super-heroes-tdd-mocha/test/client/spec/controllers/NavbarCtrl.js
@@ -1,2 +1,17 @@
 // test/client/spec/controllers/NavbarCtrl.js
 
+suite( 'Testing NavbarCtrl Controller:', function() {
+
+  var NavbarCtrl;
+
+  setup( module('marvelSuperHeroesApp') );
+
+  setup( inject(function($controller) {
+    NavbarCtrl = $controller('NavbarCtrl', {});
+  }));
+
+  test( 'if it is present', function() {
+    assert.isDefined(NavbarCtrl);
+  });
+
+});
