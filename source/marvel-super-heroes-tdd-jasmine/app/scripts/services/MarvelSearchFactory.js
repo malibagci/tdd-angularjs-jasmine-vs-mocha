5cbf8c821b95b2efb54403b84888025a55622945
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 11:18:45 +0200

    tdd-jasmine: added searching functionallity in MarvelSearchFactory; green phase;

--- source/marvel-super-heroes-tdd-jasmine/app/scripts/services/MarvelSearchFactory.js
+++ source/marvel-super-heroes-tdd-jasmine/app/scripts/services/MarvelSearchFactory.js
@@ -11,10 +11,16 @@ angular.module( 'app.services' )
     factory.searchResults = [];
     factory.search = function() {
 
-      var fetch = $http({
+      var request = $http({
         method: 'GET',
         url: 'http://gateway.marvel.com:80/v1/public/characters?' +
           'limit=20' + '&nameStartsWith=Hulk' + '&apikey=' + apikey
+      });
+
+      request.success( function(data) {
+        angular.forEach(data.data.results, function(result) {
+          factory.searchResults.push(result);
+        });
       });
 
     };
