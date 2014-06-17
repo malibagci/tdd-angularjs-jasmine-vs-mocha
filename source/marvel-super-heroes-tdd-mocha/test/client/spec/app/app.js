773c34655ff78c4d9e4ea5e280045b6bad250a16
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-17 19:18:38 +0200

    tdd-mocha: added regression test for app dependency to ngRoute; still green phase;

--- source/marvel-super-heroes-tdd-mocha/test/client/spec/app/app.js
+++ source/marvel-super-heroes-tdd-mocha/test/client/spec/app/app.js
@@ -1,16 +1,24 @@
 suite( 'Testing: App Module', function() {
 
   var module;
+  var deps;
+
+  var hasModule = function(moduleName) {
+    return deps.indexOf(moduleName) >= 0;
+  }
 
   setup( function() {
     module = angular.module( 'marvelSuperHeroesApp' );
+    deps = module.value('marvelSuperHeroesApp').requires;
   });
 
-  test( 'should validate that the module "marvelSuperHeroesApp" is present', 
+  test( 'if the module "marvelSuperHeroesApp" is present', 
     function() {
-
       assert.isDefined(module);
+  });
 
+  test( 'if the app has a registered dependency to "ngRoute"', function() {
+    assert.ok(hasModule('ngRoute'));
   });
 
 });
