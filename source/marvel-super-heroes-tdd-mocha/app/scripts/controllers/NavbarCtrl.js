e7b963cc90e803cc986690bac3bd7994b4559283
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-18 12:25:41 +0200

    tdd-mocha: added functionality for searching; it should only be triggered when the input has as least 4 characters; green phase;

--- source/marvel-super-heroes-tdd-mocha/app/scripts/controllers/NavbarCtrl.js
+++ source/marvel-super-heroes-tdd-mocha/app/scripts/controllers/NavbarCtrl.js
@@ -4,8 +4,10 @@ angular.module('app.controllers')
   .controller('NavbarCtrl', function($scope, MarvelSearchFactory) {
     $scope.searchString = '';
 
-    $scope.$watch('searchString', function() {
-      MarvelSearchFactory.search();
+    $scope.$watch('searchString', function(value) {
+      if(value.length >= 4) {
+        MarvelSearchFactory.search();
+      }
     });
   });
 
