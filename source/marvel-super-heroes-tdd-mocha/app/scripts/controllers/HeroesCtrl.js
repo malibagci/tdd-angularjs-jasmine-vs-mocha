8fa05aa70cbb30d2e7b9c41db5cddc66e9f13ddc
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-18 13:14:57 +0200

    tdd-mocha: added data-binding between heroes in HeroesCtrl and HeroesFactory; green phase;

--- source/marvel-super-heroes-tdd-mocha/app/scripts/controllers/HeroesCtrl.js
+++ source/marvel-super-heroes-tdd-mocha/app/scripts/controllers/HeroesCtrl.js
@@ -1,10 +1,11 @@
 // app/scripts/controllers/HeroesCtrl.js
 
 angular.module( 'app.controllers' )
-  .controller( 'HeroesCtrl', function($scope, MarvelSearchFactory) {
-    $scope.searchResults = MarvelSearchFactory.searchResults;
-    $scope.isSearching = MarvelSearchFactory.isSearching;
-    $scope.heroes = [];
+  .controller( 'HeroesCtrl', function($scope, MarvelSearchFactory, 
+    HeroesFactory) {
+      $scope.searchResults = MarvelSearchFactory.searchResults;
+      $scope.isSearching = MarvelSearchFactory.isSearching;
+      $scope.heroes = HeroesFactory.heroes;
 
-    $scope.addToFavorites = function() {};
+      $scope.addToFavorites = function() {};
   });
