619830d13f538c6058528a3ddeff36ff8aee507d
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-20 15:55:27 +0200

    tdd-mocha: added dependency to app.directives; green phase;

--- source/marvel-super-heroes-tdd-mocha/app/scripts/app.js
+++ source/marvel-super-heroes-tdd-mocha/app/scripts/app.js
@@ -3,12 +3,14 @@
 angular.module( 'app.controllers', [] );
 angular.module( 'app.services', [] );
 angular.module( 'app.filters', [] );
+angular.module( 'app.directives', [] );
 
 angular.module( 'marvelSuperHeroesApp', [
   'ngRoute',
   'app.controllers',
   'app.services',
   'app.filters',
+  'app.directives',
   'ngResource',
   'ngSanitize',
   'ngAnimate',
