8b631f95a32ce386eead0bc5eeb7e97403e9c146
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-20 10:15:42 +0200

    tdd-jasmine: added 'Hero' directive; green phase;

---
+++ source/marvel-super-heroes-tdd-jasmine/app/scripts/directives/Hero.js
@@ -0,0 +1,11 @@
+// app/scripts/directives/Hero.js
+
+angular.module( 'app.directives' )
+  .directive( 'hero', function() {
+
+    return {
+      template: '<div></div>',
+      restrict: 'E'
+    };
+
+  });
