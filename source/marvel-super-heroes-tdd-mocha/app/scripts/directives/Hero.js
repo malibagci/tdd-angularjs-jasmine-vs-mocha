32034c91b3b30c3ab449e0e2ed5764d949010fd5
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-20 16:01:15 +0200

    tdd-mocha: added implementation for the Hero diretive; green phase;

---
+++ source/marvel-super-heroes-tdd-mocha/app/scripts/directives/Hero.js
@@ -0,0 +1,9 @@
+// app/scripts/directives/Hero.js
+
+angular.module( 'app.directives' )
+  .directive( 'hero', function() {
+    return {
+      template: '<div></div>',
+      restrict: 'E'
+    }
+  });
