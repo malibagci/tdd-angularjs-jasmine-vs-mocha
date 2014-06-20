20bb44d9c24eb53df9d1065a00629fdde7ad5051
Sebastian Slamanig <sebastian.slamanig@gmail.com>
2014-06-20 11:58:58 +0200

    tdd-jasmine: added test to Hero directive, it should contain the givne name of a hero; red phase;

--- source/marvel-super-heroes-tdd-jasmine/test/client/spec/directives/Hero.js
+++ source/marvel-super-heroes-tdd-jasmine/test/client/spec/directives/Hero.js
@@ -11,12 +11,19 @@ describe( 'Testing Hero Directive:', function() {
   beforeEach( inject( function(_$compile_, _$rootScope_) {
     $compile = _$compile_;
     $scope = _$rootScope_.$new();
+    $scope.hero = {};
 
     hero = $compile( '<hero></hero>' )($scope);
   }));
 
   it( 'should return at least one html div', function() {
     expect( hero.html() ).toContain( 'div' );
+  });
+
+  it( 'should contain the name of a hero', function() {
+    $scope.hero.name = 'Hulk';
+    $scope.$apply();
+    expect( hero.html() ).toContain( 'Hulk' );
   });
 
 });
